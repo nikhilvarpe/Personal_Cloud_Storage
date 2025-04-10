747a62a52f17d7c43f4eb28f58feb047abed1bdd
Nikhil_Varpe <nikhilvarpe11@gmail.com>
2025-04-11 01:50:22 +0530

    Add Firebase config from .env file

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCNJjVUlQan6UURScEdZqTFrmAnWjyXplc",
-  authDomain: "cloud-scheduling-project.firebaseapp.com",
-  databaseURL: "https://cloud-scheduling-project-default-rtdb.firebaseio.com",
-  projectId: "cloud-scheduling-project",
-  storageBucket: "cloud-scheduling-project.firebasestorage.app",
-  messagingSenderId: "1056568865075",
-  appId: "1:1056568865075:web:a127f3601decdea0ee8e46",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = app.auth();
